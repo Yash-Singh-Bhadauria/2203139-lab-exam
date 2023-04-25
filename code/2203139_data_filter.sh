7cc8a3be1e2d7cababfe0dc3669e80143983948e
Yash-Singh-Bhadauria <129713823+Yash-Singh-Bhadauria@users.noreply.github.com>
2023-04-25 12:09:51 +0530

    I did it

--- code/2203139_data_filter.sh
+++ code/2203139_data_filter.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-echo "Give file name:"
-read file_name
+file_name=$1
 
-echo "Give the name of field:"
-read field_name
+field_name=$2
 
-echo "Give field value:"
-read field_value
+field_value=$3
 cat  $filed_name $filed_value  $file_name>${file_name:0:2}-$filed_name-$filed_value.csv
 head -n 1 $file_name>>${file_name:0:2}-$field_name-$field_value.csv
 cat $file_name | grep -w $field_value>>${file_name:0:2}-$field_name-$field_value.csv
